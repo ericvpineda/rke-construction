dc09d64780d730e6a227df149e47bdb7a8a6e7b5
Eric Pineda <ericvpineda@berkeley.edu>
2023-09-11 17:18:41 -0700

    added -- ai generated logo from app.logo.com

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,6 +2,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Icons } from './Icons';
 
 const navigation = [
   { name: "Home", href: "/", current: true },
@@ -35,11 +36,7 @@ export default function Nav() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
+                  <Icons.logo/>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
