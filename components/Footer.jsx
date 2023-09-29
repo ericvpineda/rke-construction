38d18ac332b5c330b98178608b04ea5f5547a288
Eric Pineda <ericvpineda@berkeley.edu>
2023-09-29 14:29:55 -0700

    updated -- Fotter.jsx responsive styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,20 +2,20 @@ import "@styles/css/footer.css"
 
 export default function Footer() {
   return (
-    <div className="flex justify-center p-10 sm:p-0">
+    <div className="flex justify-center pl-10 m-0 w-full">
         <footer id="footer">
-          <div class="inner flex justify-center">
+          <div className="inner flex justify-center">
             <section>
               <h2>Get in touch</h2>
               <form id="inquiry-form" method="post" action="">
-                <div class="fields">
+                <div className="fields">
                   <input
                     type="hidden"
                     name="_subject"
                     value="New email from Personal Website!"
                   />
                   <input type="hidden" name="_captcha" value="false" />
-                  <div class="field half">
+                  <div className="field half">
                     <input
                       type="text"
                       name="name"
@@ -24,7 +24,7 @@ export default function Footer() {
                       required
                     />
                   </div>
-                  <div class="field half">
+                  <div className="field half">
                     <input
                       type="email"
                       name="email"
@@ -33,7 +33,7 @@ export default function Footer() {
                       required
                     />
                   </div>
-                  <div class="field">
+                  <div className="field">
                     <textarea
                       name="message"
                       id="message"
@@ -42,19 +42,19 @@ export default function Footer() {
                     ></textarea>
                   </div>
                 </div>
-                <ul class="actions">
+                <ul className="actions">
                   <li>
                     <input
                       id="submit-btn"
                       type="submit"
                       value="Send"
-                      class="primary"
+                      className="primary"
                     />
                   </li>
                 </ul>
               </form>
             </section>
-            <ul class="copyright flex justify-center">
+            <ul className="copyright flex justify-center">
               <li>&copy; RKE Construction Inc. All rights reserved</li>
             </ul>
           </div>
--- components/MissionStatement.jsx
+++ components/MissionStatement.jsx
@@ -5,7 +5,7 @@ export default function MissionStatement() {
     <div className='container-style pb-12 flex flex-col justify-center text-center tracking-light'>
         <h1 className='mb-6 text-xl'>RKE Construction Inc.</h1>
         <h2 className='mb-10 font-bold text-xl md:text-2xl lg:text-3xl uppercase border-b-2 pb-5 tracking-tight'>Transforming Your Blueprints into Reality</h2>
-        <p className='max-w-2xl font-light mx-auto'>We bring your architectural visions to life, crafting spaces that inspire, innovate, and elevate your lifestyle. Our experienced team combines expertise and creativity to transform your dreams into reality, one brick at a time."</p>
+        <p className='max-w-2xl font-light mx-auto'>We bring your architectural visions to life, crafting spaces that inspire, innovate, and elevate your lifestyle. Our experienced team combines expertise and creativity to transform your dreams into reality, one brick at a time.</p>
     </div>
   )
 }
