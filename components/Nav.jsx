608722715f233d8619e88fce8d3f835ecf59feb7
Eric Pineda <ericvpineda@berkeley.edu>
2023-09-29 22:29:46 -0700

    fixed bug -- Nav.jsx logo overlaps call button, updated Logo to be aligned to center of whole nav, call button styling

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,13 +3,13 @@ import { Disclosure, Transition } from "@headlessui/react";
 import { Icons } from "./Icons";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 
 const navigation = [
   { name: "Home", href: "/", current: true },
-  { name: "About", href: "/about", current: false },
   { name: "Projects", href: "/projects", current: false },
   { name: "Pricing", href: "/pricing", current: false },
+  { name: "About", href: "/about", current: false },
 ];
 
 function classNames(...classes) {
@@ -17,62 +17,67 @@ function classNames(...classes) {
 }
 
 export default function Nav() {
-  const pathname = usePathname()
+  const pathname = usePathname();
   return (
     <Disclosure as="nav" className="theme_blue">
-      {({ open }) => (
+      {() => (
         <>
-          <div className="mx-auto max-w-[84rem] px-2 sm:px-6 lg:px-8 pr-4">
-            <div className="max-w-7xl text-white pt-5 mx-auto">
-              <span className="flex justify-end">
-                (xxx)-xxx-xxxx | xxx@gmail.com
-              </span>
-            </div>
-            <div className="relative flex h-12 py-10 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden ">
-                {/* Mobile menu button*/}
-
-                <Disclosure.Button className="pr-5 relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:text-white !ring-0 ">
-                  <Icons.logo />
-                </Disclosure.Button>
+          <div className="mx-auto max-w-[84rem] px-2 sm:px-6 lg:px-8 pr-4 flex items-center">
+            <Link
+              href="/"
+              className="flex flex-shrink-0 items-center pl-10 sm:pl-0 hidden sm:block"
+            >
+              <Icons.logo />
+            </Link>
+            <div className="flex flex-col w-full items-end">
+              <div className="max-w-7xl text-white pt-5">
+                <span className="flex justify-end">
+                  (xxx)-xxx-xxxx | xxx@gmail.com
+                </span>
               </div>
+              <div className="relative flex h-12 py-10 items-center justify-end w-full sm:w-0">
+                <div className="flex items-center sm:hidden ">
+                  {/* Mobile menu button*/}
 
-              {/* Desktop view  */}
-              <div className="flex flex-1 items-center justify-center sm:justify-between sm:items-center">
-                <Link
-                  href="/"
-                  className="flex flex-shrink-0 items-center pl-10 sm:pl-0 hidden sm:block"
-                >
-                  <Icons.logo />
-                </Link>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          pathname == item.href
-                            ? "border-solid border-4 border-white text-white "
-                            : "text-gray-300 hover:text-white text-white hover:bg-white",
-                          "px-3 py-2 text-sm font-medium rounded-none border-none"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
+                  <Disclosure.Button className="pr-5 relative inline-flex items-center justify-start rounded-md p-2 text-gray-400 hover:text-white !ring-0 ">
+                    <Icons.logo />
+                  </Disclosure.Button>
+                </div>
+
+                {/* Desktop view  */}
+                <div className="flex flex-1 items-center justify-center sm:justify-between sm:items-center">
+                  <div className="hidden sm:ml-6 sm:block">
+                    <div className="flex space-x-4">
+                      {navigation.map((item) => (
+                        <Link
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            pathname == item.href
+                              ? "border-solid border-white border-b-2 hover:text-white"
+                              : " hover:text-white  hover:border-solid hover:border-b-2 hover:border-gray-300",
+                            "px-3 py-2 text-md font-medium rounded-none text-white"
+                          )}
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          {item.name}
+                        </Link>
+                      ))}
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="">
-                <Link
-                  href="/"
-                  className={buttonVariants({ variant: "outline" })}
-                >
-                  CALL NOW
-                </Link>
+                <div className="">
+                  <Link
+                    href="/"
+                    className={classNames(
+                      buttonVariants({ variant: "outline" }),
+                      "ml-7 whitespace-nowrap"
+                    )}
+                  >
+                    CALL NOW
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
