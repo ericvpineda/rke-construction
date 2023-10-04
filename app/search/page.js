54cc1a6e5b7cfcd1b67d0e042fd6f9fec9b7258a
Eric Pineda <ericvpineda@berkeley.edu>
2023-10-03 23:45:41 -0700

    updated -- search page added spinner when page is fetching data

--- app/search/page.js
+++ app/search/page.js
@@ -28,7 +28,6 @@ export default function Search() {
 
   useEffect(() => {
     if (entry?.isIntersecting) {
-      console.log("DEBUG: fetching next page...")
       fetchNextPage();
     }
   }, [entry, fetchNextPage]);
@@ -36,9 +35,9 @@ export default function Search() {
   const results = data?.pages.flatMap((page) => page) ?? [];
 
   return (
-    <div className="max-w-7xl mx-auto mb-10">
+    <div className="max-w-7xl mx-auto mb-10 flex justify-center flex-col">
       {/* Search Bar */}
-      <div className="flex flex-1 items-center justify-center px-10 md:px-0 my-10">
+      <div className="flex flex-1 items-center justify-center px-10 md:px-0 my-10 flex">
         <div className="w-full max-w-lg lg:max-w-lg">
           <label htmlFor="search" className="sr-only">
             Search
@@ -61,13 +60,45 @@ export default function Search() {
         </div>
       </div>
 
-      {/* Images Search  */}
-      <div className="mx-20 grid sm:grid-cols-2 md:grid-cols-4 gap-4">
-        {results.map((elem, index) => {
-          if (elem !== null && elem !== undefined) {
-            if (index >= results.length - 4) {
+      {isFetchingNextPage ? (
+        <div role="status" className="self-center">
+          <svg
+            aria-hidden="true"
+            class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span class="sr-only">Loading...</span>
+        </div>
+      ) : (
+        <div className="mx-20 grid sm:grid-cols-2 md:grid-cols-4 gap-4">
+          {results.map((elem, index) => {
+            if (elem !== null && elem !== undefined) {
+              if (index >= results.length - 4) {
+                return (
+                  <div className="" key={elem.id} ref={ref}>
+                    <Image
+                      src={elem.url}
+                      height={500}
+                      width={500}
+                      className="max-h-[15rem] min-h-[15rem] w-full rounded-md"
+                    />
+                  </div>
+                );
+              }
+
               return (
-                <div className="" key={elem.id} ref={ref}>
+                <div className="" key={elem.id}>
                   <Image
                     src={elem.url}
                     height={500}
@@ -77,20 +108,9 @@ export default function Search() {
                 </div>
               );
             }
-
-            return (
-              <div className="" key={elem.id}>
-                <Image
-                  src={elem.url}
-                  height={500}
-                  width={500}
-                  className="max-h-[15rem] min-h-[15rem] w-full rounded-md"
-                />
-              </div>
-            );
-          }
-        })}
-      </div>
+          })}
+        </div>
+      )}
     </div>
   );
 }
