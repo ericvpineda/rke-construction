131a70b8cfcac50e53687821fd900bd3325ef428
Eric Pineda <ericvpineda@berkeley.edu>
2023-12-02 15:17:14 -0800

    fixed bug -- middleware.js verifyAuth trycatch would not redirect to login page successful on expired jwt token

--- middleware.js
+++ middleware.js
@@ -4,22 +4,19 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   // Note: Middleware is running on edge runtime
   const token = req.cookies.get("user-token")?.value;
-  try {
-    const verifiedToken = token && (await verifyAuth(token));
+  const verifiedToken =
+    token && (await verifyAuth(token).catch((err) => console.log(err)));
 
-    // Case: User verified and on login page or accessing api post route
-    if (verifiedToken) {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
-
-    return NextResponse.next();
-  } catch (err) {
-    return NextResponse.json({
-      success: false,
-      message: err.message,
-      status: 500,
-    });
+  if (!verifiedToken && req.nextUrl.pathname.startsWith("/login")) {
+    return;
   }
+
+  // Case: User verified and on login page or accessing api post route
+  if (verifiedToken) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
 }
 
 // Middlware activates only on routes below
